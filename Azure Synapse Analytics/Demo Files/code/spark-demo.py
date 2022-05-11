6b2b2440120d8e59576812e9160d8a6a4c09e176
Huma Kalayci <humakalayci@gmail.com>
2022-05-11 13:25:13 +0200

    Update spark-demo.py

--- Azure Synapse Analytics/Demo Files/code/spark-demo.py
+++ Azure Synapse Analytics/Demo Files/code/spark-demo.py
@@ -1,3 +1,25 @@
 %%pyspark
 df = spark.read.load('abfss://dlfilesystem0522@datalakegen20522.dfs.core.windows.net/synapse/NYCTripSmall.parquet', format='parquet')
-df.write.mode("overwrite").saveAsTable("default.YourTableName")
+df.printSchema()
+display(df.limit(10))
+
+
+
+spark.sql("CREATE DATABASE IF NOT EXISTS nyctaxi")
+df.repartition(1)
+df.write.mode("overwrite").saveAsTable("nyctaxi.trip")
+spark.sql("Select count(*) from nyctaxi.trip")
+
+
+
+df = spark.sql("""
+   SELECT PassengerCount,
+       SUM(TripDistanceMiles) as SumTripDistance,
+       AVG(TripDistanceMiles) as AvgTripDistance
+   FROM nyctaxi.trip
+   WHERE TripDistanceMiles > 0 AND PassengerCount > 0
+   GROUP BY PassengerCount
+   ORDER BY PassengerCount
+""") 
+display(df)
+df.write.saveAsTable("nyctaxi.passengercountstats")
